e3fb411be4326b33e61d9dd627196f176c8df9d6
Leo Rodriguez <leorod18@yahoo.com>
2025-03-06 21:36:11 -0500

    complete

--- todo-app/todo.js
+++ todo-app/todo.js
@@ -3,6 +3,9 @@
 const textInput = document.getElementById("textInput");
 // do the same for the button call it: addButton
 const addButton = document.getElementById("addButton");
+//get the element and store in
+      // variable called taskList
+const taskList = document.getElementById("taskList");
 
 // create function called addTask, that alerts hello
 // function lets us reuse code blocks
@@ -12,9 +15,39 @@ const addButton = document.getElementById("addButton");
 // change name and alert
 
 function addTask() {
-  // textInput.value
-  // create a list with this value
+  // STORE INPUT VALUE
+  const input = textInput.value;
 
+  // CREATE li TAG
+  const newLi = document.createElement("li");
+  // CREATE "button" TAG and call deleteBtn
+  const deleteBtn= document.createElement("button");
+
+  // FILL LI TAG WITH TEXT
+  newLi.textContent = input;
+  // ADD CSS
+  newLi.classList.add("li-style");
+  // FILL BUTTON WITH "X"
+  deleteBtn.textContent = "X";
+  deleteBtn.style.color = "red";
+  deleteBtn.style.marginLeft = "5px";
+
+  // create function called deleteTask, console.log(input)
+
+  function deleteTask() {
+      // REMOVE FROM UL
+    taskList.removeChild(newLi);
+  }
+
+  // add a click event to the button
+  deleteBtn.addEventListener("click", deleteTask);
+
+  // append the btn to newLi; has to be on top
+  // PARENT ADD CHILD
+  newLi.appendChild(deleteBtn);
+  taskList.appendChild(newLi);
+
+  textInput.value = "";
 }
 
 addButton.addEventListener(
