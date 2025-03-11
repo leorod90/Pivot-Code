b4a1e77c8482757d6ae7931967eac4a3496c9487
Leo Rodriguez <leorod18@yahoo.com>
2025-03-11 19:06:29 -0400

    seperate days of ecom

---
+++ shopping-app/Mar11/index.js
@@ -0,0 +1,40 @@
+// create a list called fake products
+// create and obj: {}; id: 0, name, price: number, image
+// 3 items
+const fakeProducts = [
+  // batman item
+  {
+    id: 0,
+    name: "Batman Plush",
+    price: 20,
+    image: "https://m.media-amazon.com/images/I/61YiGW9dHgL._AC_UF894,1000_QL80_.jpg"
+  },
+  // spiderman item
+  {
+    id: 1,
+    name: "Spiderman Plush",
+    price: 15,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCe_fz1nlNMKmgj_PHT6bZm0yGrU-Z_ja16Q&s"
+  }
+];
+
+// create a function called onLoad
+// console log fake products
+function onLoadFunction() {
+  // get product list is
+  const productsList = document.getElementById("products-list");
+  // console.log(fakeProducts);
+  // loop through your data
+  for (let i = 0; i < fakeProducts.length; i++) {
+    const text = document.createElement("p");
+    text.textContent = fakeProducts[i].name;
+    productsList.append(text);
+  }
+
+}
+
+// window.onload = onLoad;
+window.addEventListener("load", onLoadFunction);
+
+
+// https://airtable.com/app74b02cdfIPSsyG/pagLR0LgXLSCDrMRU/form
