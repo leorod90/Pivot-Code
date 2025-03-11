e9a5b66dbc935e93f7fbdc19b80ff19b0f948d7f
Leo Rodriguez <leorod18@yahoo.com>
2025-03-11 19:51:14 -0400

    delet html

--- shopping-app/Mar10/index.js
+++ shopping-app/Mar10/index.js
@@ -26,25 +26,12 @@ function onLoadFunction() {
   // console.log(fakeProducts);
   // loop through your data
   for (let i = 0; i < fakeProducts.length; i++) {
-    // test
     const text = document.createElement("p");
     text.textContent = fakeProducts[i].name;
-    // productsList.append(text);
-
-    // create a div in js and call card div
-    const cardDiv = document.createElement("div");
-    // add a class card and item-width
-    cardDiv.classList.add("card");
-    cardDiv.classList.add("item-width");
-
-    cardDiv.append(text);
-    productsList.append(cardDiv);
+    productsList.append(text);
   }
 
 }
 
 // window.onload = onLoad;
 window.addEventListener("load", onLoadFunction);
-
-
-// https://airtable.com/app74b02cdfIPSsyG/pagLR0LgXLSCDrMRU/form
