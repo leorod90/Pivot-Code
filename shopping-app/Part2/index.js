16c88221aa28856fadf3caff36a1711b35f60852
Leo Rodriguez <leorod18@yahoo.com>
2025-03-11 21:42:04 -0400

    add description

--- shopping-app/Part2/index.js
+++ shopping-app/Part2/index.js
@@ -6,7 +6,7 @@ const fakeProducts = [
   {
     id: 0,
     name: "Batman Plush",
-    description: "This is a cool batman plush",
+    description: "This is a cool batman plush: Did you hear that? They've shut down the main reactor. We'll be destroyed for sure. This is madness! We're doomed! There'll be no escape for the Princess this time. What's that? Artoo! Artoo-Detoo, where are you? At last! Where have you been? They're heading in this direction.",
     price: 20,
     image: "https://m.media-amazon.com/images/I/61YiGW9dHgL._AC_UF894,1000_QL80_.jpg"
   },
@@ -14,7 +14,7 @@ const fakeProducts = [
   {
     id: 1,
     name: "Spider-Man Plush",
-    description: "This is a rare Spider-Man",
+    description: "This is a rare Spider-Man: Not for long. A fighter that size couldn't get this deep into space on its own. Well, he ain't going to be around long enough to tell anyone about us. Look at him. He's headed for that small moon. I think I can get him before he gets there...he's almost in range. That's no moon! It's a space station.",
     price: 15,
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCe_fz1nlNMKmgj_PHT6bZm0yGrU-Z_ja16Q&s"
   }
@@ -42,9 +42,9 @@ function onLoadFunction() {
     const image = document.createElement("img");
     // add src, class: card-img-top and alt
     image.src = fakeProducts[i].image;
-    image.alt = fakeProducts[i].name;
     image.classList.add("card-img-top");
-
+    image.alt = fakeProducts[i].name;
+    
     // create div class card-body
     const cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
@@ -54,11 +54,17 @@ function onLoadFunction() {
     title.classList.add("card-title");
     title.textContent = fakeProducts[i].name; // change element to title
 
-    // create p tag, class card-text
+    // create p tag, class card-text, 
+    // content description
+    const description = document.createElement("p");
+    description.classList.add("card-text");
+    description.textContent = fakeProducts[i].description;
+    
     // create a tag, class btn btn-primary
 
     // create body div; append all 3
     cardBody.appendChild(title);
+    cardBody.appendChild(description);
 
     // structure html
     cardDiv.appendChild(image);
@@ -66,7 +72,6 @@ function onLoadFunction() {
     cardDiv.append(cardBody);
     productsList.append(cardDiv);
   }
-
 }
 
 // window.onload = onLoad;
