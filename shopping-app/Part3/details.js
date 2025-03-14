13ef14b7fbb41ad039d7f264f8ac32e4fea5b969
Leo Rodriguez <leorod18@yahoo.com>
2025-03-13 21:41:02 -0400

    complete

--- shopping-app/Part3/details.js
+++ shopping-app/Part3/details.js
@@ -2,9 +2,13 @@ import { fakeProducts } from "./data.js";
 
 // create the func
 function onLoadFunction() {  
+  // GET URL PARAMETERS                      CURRENT URL
   const urlParams = new URLSearchParams(window.location.search);
   const productId = urlParams.get("id");
-  console.log(fakeProducts[productId]);
+  // basic version
+  const item = fakeProducts[productId];
+
+  console.log(item);
 }
 
 window.addEventListener("load", onLoadFunction);
